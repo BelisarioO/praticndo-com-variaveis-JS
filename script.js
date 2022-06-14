9f01a63fdc265967193e8a3c7f215a750ef95ae0
bruno belisario <brunobelisarioxp18@gmail.com>
2022-06-14 14:09:11 -0300

    pratica com JS

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome          = "Bruno"
+let sobrenome     = "Belisario" 
+let anoNascimento = 2022 - 2003
+let cidade        = "Cáceres-MT"
+
+console.log(`Eu sou o ${nome} ${sobrenome} tenho ${anoNascimento} anos e moro em ${cidade}`)
